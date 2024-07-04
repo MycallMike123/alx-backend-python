2eada8161f615210e893d1372b068421b325e5ab
Michael Mwangi <michaelmichire2016@gmail.com>
2024-07-03 23:15:44 -0700

    A type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier

---
+++ 8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Module that takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Function to create and return a function that multiplies
+    a float by the specified multiplier
+    """
+    def multiply(n: float) -> float:
+        """Function that multiplies a float by multiplier"""
+        return n * multiplier
+    return multiply
