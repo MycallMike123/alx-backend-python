d9c4cbed6194a1d2a13b060a9f3c2fde8c5871fd
Michael Mwangi <michaelmichire2016@gmail.com>
2025-05-28 21:55:24 +0300

    models for users, messages, and conversations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -14,6 +14,7 @@ class CustomUser(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
+    password = models.CharField(max_length=128)
 
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
     USERNAME_FIELD = 'email'
