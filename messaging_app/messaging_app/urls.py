e9748ff9a9a5890a656054d003513e1e8f7f4e4a
Michael Mwangi <michaelmichire2016@gmail.com>
2025-06-01 10:52:39 +0300

    configure URLS for the conversations and messages

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # Include the chats API URLs under /api/
+    path('api/', include('messaging_app.chats.urls')),
 ]
